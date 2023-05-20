ebc635e2295f6646062a888b96796c045265657e
Yuna Pahk <yunapahk@gmail.com>
2023-05-20 14:46:11 -0700

    push

--- Desktop/GA/unit1/week3/Deliverable/hogfarts/app.js
+++
@@ -1,8 +0,0 @@
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
-
-
-const container = $("#container")
-console.log(container)
-
